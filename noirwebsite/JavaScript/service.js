fec5a736c3fbfb4c2756d4fe8a465e3aea5e2ef2
VincentNjele <njelevincent0@gmail.com>
2022-06-17 10:21:40 -0700

    I have made changes

--- noirwebsite/JavaScript/service.js
+++ noirwebsite/JavaScript/service.js
@@ -1,54 +1,54 @@
 
- 
 
- window.addEventListener('scroll',show);
 
- function show() {
+window.addEventListener('scroll', show);
 
-     var showData = document.querySelectorAll('.show');
+function show() {
 
-     for(var i=0; i<showData.length; i++) {
+    var showData = document.querySelectorAll('.show');
 
-         var windowHeight = window.innerHeight;
-         var showTop = showData[i].getBoundingClientRect().top;
-         var showPoint = 150;
-         
-         if(showTop <windowHeight-showPoint) {
-            
-           
-             showData[i].classList.add('active');
-            
-         } else {
-             showData[i].classList.remove('active');
-         }
-     }
- }
+    for (var i = 0; i < showData.length; i++) {
 
- function displayValue() {
- let displayValue = document.querySelectorAll('.item-stats-count');
- let interval = 5000;
+        var windowHeight = window.innerHeight;
+        var showTop = showData[i].getBoundingClientRect().top;
+        var showPoint = 150;
 
- console.log(displayValue)
- displayValue.forEach(value => {
- let start = 0;
- let end = parseInt(value.getAttribute("data-val"));
+        if (showTop < windowHeight - showPoint) {
 
- let duration = Math.floor(interval/end);
 
- let counter = setInterval(function(){
+            showData[i].classList.add('active');
 
-     start +=1;
-     value.textContent = start;
+        } else {
+            showData[i].classList.remove('active');
+        }
+    }
+}
 
-     if(start == end) {
-         clearInterval(counter);
-     }
- }, duration);
+function displayValue() {
+    let displayValue = document.querySelectorAll('.item-stats-count');
+    let interval = 5000;
 
- })
+    console.log(displayValue)
+    displayValue.forEach(value => {
+        let start = 0;
+        let end = parseInt(value.getAttribute("data-val"));
 
- }
- displayValue();
+        let duration = Math.floor(interval / end);
+
+        let counter = setInterval(function () {
+
+            start += 1;
+            value.textContent = start;
+
+            if (start == end) {
+                clearInterval(counter);
+            }
+        }, duration);
+
+    })
+
+}
+displayValue();
 
 
 //  const swiper = new Swiper('.swiper', {
@@ -65,32 +65,178 @@
 
 //     });
 
-  
-    // function moveImage() {
 
-    //     var movePosition = document.getElementById('image');
-    //     window.addEventListener('scroll', function() {
-            
-    //         movePosition.style.backgroundPosition = +window.pageYOffset + 'px';
-    //     })
-    // }
+// function moveImage() {
 
-    // moveImage();
+//     var movePosition = document.getElementById('image');
+//     window.addEventListener('scroll', function() {
 
-    $(window).scroll(function() {
-        var scrollTop = $('html,body').scrollTop();
-        console.log(scrollTop)
-        $('.container').css({'background-positionY':+ (-scrollTop/10) + 'px'});
+//         movePosition.style.backgroundPosition = +window.pageYOffset + 'px';
+//     })
+// }
+
+// moveImage();
+
+$(window).scroll(function () {
+    var scrollTop = $('html,body').scrollTop();
+    console.log(scrollTop)
+    $('.container').css({ 'background-positionY': + (-scrollTop / 10) + 'px' });
+});
+
+window.onscroll = function () {
+    var getUl = document.getElementById('getUl');
+    var nav = document.getElementById('nav');
+
+    if (window.pageYOffset > 560) {
+
+
+        nav.style.backgroundColor = '#000000';
+        nav.style.boxShadow = '1px 1px 1px orangered';
+        getUl.style.backgroundColor = '#000000';
+    } else {
+        nav.style.backgroundColor = 'transparent';
+        getUl.style.backgroundColor = '#2c3e50';
+        nav.style.boxShadow = 'none';
+    }
+}
+
+// function showNav() {
+
+//     var nav = document.querySelector('.menu-icon');
+//     var isOpen = false;
+
+//     nav.addEventListener('click', function() {
+//         if(!isOpen) {
+//           nav.classList.add('open');
+//           isOpen = true;
+//           alert('Please');
+
+//           var tagName = document.querySelector('.list');
+
+//           tagName.style.opacity = 1;
+
+//         }else {
+
+//             nav.classList.remove('open');
+//             isOpen = false; 
+
+//         }
+
+//     });
+
+
+// }
+// showNav();
+
+function displayModal() {
+
+    var registerBtn = document.querySelector('#register-with-us');
+    var closeBtn = document.querySelector('#close');
+    var modalBtn = document.querySelector('.Register-box');
+    registerBtn.addEventListener('click', function () {
+        modalBtn.classList.add('display-modal');
     });
 
-    window.onscroll = function(){
+    closeBtn.addEventListener('click', function () {
+        modalBtn.classList.remove('display-modal');
+    });
+}
 
-        var nav = document.getElementById('nav');
-        if(window.pageYOffset> 560){
-            nav.style.backgroundColor = '#000000';
-            nav.style.boxShadow = '1px 1px 1px orangered';
-        }else {
-            nav.style.backgroundColor = 'transparent';
-            nav.style.boxShadow = 'none';
-        }
-    }
+displayModal();
+
+
+
+function displayModalBook() {
+
+
+    var reg2 = document.querySelector('.book-now');
+    var closeBtn2 = document.querySelector('#close');
+    var modalBtn2 = document.querySelector('.Register-box');
+    reg2.addEventListener('click', function () {
+        modalBtn2.classList.add('display-modal');
+    });
+
+    closeBtn2.addEventListener('click', function () {
+        modalBtn2.classList.remove('display-modal');
+    });
+}
+
+displayModalBook();
+
+
+
+// const links = document.querySelectorAll('.nav-link');
+//     links.forEach(link => {
+
+//        if(link.href === $(this).attr('href')) {
+//         $(this).addClass('active-link');
+//        }
+//         link.addEventListener('click', function(){
+//             $(this).addClass('active-link');
+//         })
+//     })
+
+
+// $('a').click(function(){
+//     $('a').css('background-color', '');
+//     $(this).css('background-color','#1b9bff;')
+// })
+
+// const section = document.querySelectorAll('section');
+// function addActiveLink (){
+
+//     // let len = section.length;
+
+//     // while(--len && window.scrollY <section[len].offsetTop){}
+//     // links.forEach(link=>{
+//     //     link.classList.remove('active-link');
+//     //     link.classList.add('active-link');
+//     // })
+
+
+//     for(var i=0; i<section.length; i++) {
+
+//         var windowHeight = window.innerHeight;
+//         var showTop = section[i].getBoundingClientRect().top;
+//         var showPoint = 0;
+
+//         if(showTop <windowHeight-showPoint) {
+
+
+//             links[i].classList.add('active-link');
+
+//         } else {
+//             links[i].classList.remove('active-link');
+//         }
+//     }
+
+
+// }
+// addActiveLink();
+
+// window.addEventListener('scroll', addActiveLink);
+
+var addActiveLink = function () {
+
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll(".nav-link");
+    window.addEventListener("scroll", () => {
+        let current = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLi.forEach((li) => {
+            li.classList.remove("active-link");
+            if (li.classList.contains(current)) {
+                li.classList.add("active-link");
+            }
+        });
+    });
+}
+
+addActiveLink();
